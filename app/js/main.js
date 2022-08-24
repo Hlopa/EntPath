53c6e6e0f7e078c38d9c4206a4685ccd7aae7293
Olga Barishevskaya <bariolga@yandex.ru>
2022-08-24 23:51:07 +0300

    finish layout

--- app/js/main.js
+++ app/js/main.js
@@ -2,63 +2,121 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+  //=============Открытие и  закрытие меню в зедере====================//
 
   const headerProfileBtn = document.querySelector('.header-profile__btn');
-  const headerProfileMenu = document.querySelector('.header-profile__menu');  
-  const toggleMenu = function () {
-    headerProfileMenu.classList.toggle('header-profile__menu--shown')
-}
 
-headerProfileBtn.addEventListener("click", function (e) {
-  e.stopPropagation();
-  toggleMenu();
-});
+  if (headerProfileBtn) {
+    const headerProfileMenu = document.querySelector('.header-profile__menu');
 
-document.addEventListener("click", function (e) {
-  const target = e.target;
-  const its_menu = target == headerProfileMenu || headerProfileMenu.contains(target);
-  const its_btnMenu = target == headerProfileBtn;
-  const menu_is_active = headerProfileMenu.classList.contains("header-profile__menu--shown");
+    const toggleMenu = function () {
+      headerProfileMenu.classList.toggle('header-profile__menu--shown')
+    }
 
-  if (!its_menu && !its_btnMenu && menu_is_active) {
+    headerProfileBtn.addEventListener("click", function (e) {
+      e.stopPropagation();
       toggleMenu();
+    });
+
+    document.addEventListener("click", function (e) {
+      const target = e.target;
+      const its_menu = target == headerProfileMenu || headerProfileMenu.contains(target);
+      const its_btnMenu = target == headerProfileBtn;
+      const menu_is_active = headerProfileMenu.classList.contains("header-profile__menu--shown");
+
+      if (!its_menu && !its_btnMenu && menu_is_active) {
+        toggleMenu();
+      }
+    });
   }
-});
 
 
-//=====================Логика переключения шагов при записи к траектологу
+  //=============Переход к оплате====================//
 
-const createAppointmentBlock = document.querySelector('.create-appointment__block'),
-allTraektologies = createAppointmentBlock.querySelectorAll('.create-appointment__traektologies-item'),
-firstStepTab = document.getElementById('create-appointment__firstStep'),
-secondStepTab = document.getElementById('create-appointment__secondStep'),
-thirdStepTab = document.getElementById('create-appointment__thirdStep'),
-firstStepBlock = createAppointmentBlock.querySelector('.create-appointment__traektologies-list'),
-secondStepBlock = createAppointmentBlock.querySelector('.create-appointment__selected-targetolog'),
-thirdStepBlock = createAppointmentBlock.querySelector('.create-appointment__pay'),
-payBtn = createAppointmentBlock.querySelector('.pay-btn');
+  const goToPayBtn = document.querySelectorAll('.goToPay');
 
-allTraektologies.forEach(traektolog => {
-  traektolog.addEventListener("click", () => {
-    firstStepTab.classList.remove('active');
-    firstStepTab.classList.add('success');
-    secondStepTab.classList.add('active');
-    firstStepBlock.classList.add('transition-left');
-    secondStepBlock.classList.add('active')
+  goToPayBtn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      location.href = "pay-modul.html";
+    })
   })
+
+
+  //=============Логика переключения шагов при записи к траектологу====================//
+
+  const appointment__steps = document.querySelector('.appointment__steps');
+
+  if (appointment__steps) {
+    const traektologies = document.querySelectorAll('.appointment__traektolog-item'),
+      firstStepTab = document.getElementById('appointment__firstStep'),
+      secondStepTab = document.getElementById('appointment__secondStep'),
+      firstStepBlock = document.querySelector('.appointment__traektologies'),
+      secondStepBlock = document.querySelector('.appointment__selected-traektolog');
+
+    traektologies.forEach(traektolog => {
+      traektolog.addEventListener("click", () => {
+        firstStepTab.classList.remove('active');
+        firstStepTab.classList.add('success');
+        secondStepTab.classList.add('active');
+        firstStepBlock.classList.add('transition-left');
+        secondStepBlock.classList.add('active')
+      })
+    })
+  }
+
+
+  //========================= маска для инпута Дата рождения ====================//
+
+  const dateInput = document.getElementById('date-mask');
+  const dateInputGroup = document.getElementById('date-mask-group');
+
+  if (dateInput) {
+    var dateMask = IMask(
+      dateInput,
+      {
+        mask: Date,
+        min: new Date(1900, 0, 1),
+        max: new Date(2050, 0, 1),
+        lazy: true
+      });
+
+    dateInputGroup.addEventListener('click', () => {
+      var newMask = IMask(
+        dateInput,
+        {
+          mask: Date,
+          min: new Date(1900, 0, 1),
+          max: new Date(2050, 0, 1),
+          lazy: false,
+          placeholderChar: "_"
+        });
+    })
+  }
+
+
+  //========================= маска для инпута Номер телефона ====================//
+
+
+  const phoneInput = document.getElementById('phone-mask');
+  const phoneInputGroup = document.getElementById('phone-mask-group');
+
+  if (phoneInput) {
+    var phoneMask = IMask(phoneInput, {
+      mask: '+{7}(000)000-00-00',
+      lazy: true,
+    });
+
+    phoneInputGroup.addEventListener('click', () => {
+      var newphoneMask = IMask(phoneInput, {
+        mask: '+{7}(000)000-00-00',
+        lazy: false,
+        placeholderChar: '_'
+      });
+    })
+  }
 })
 
-payBtn.addEventListener('click', () => {
-   console.log(this);
-  secondStepTab.classList.remove('active');
-  secondStepTab.classList.add('success');
-
-  secondStepBlock.classList.remove('active')
-  secondStepBlock.classList.add('transition-left')
-
-  thirdStepTab.classList.add('active')
-  thirdStepBlock.classList.add('active')
-}) 
 
 //Функция для очистки зачения инпута при нажатии на крестик
 function cleanInputIcon(inputValueId, iconId) {
@@ -92,58 +150,11 @@ function togglePassword(event, inputId, showEyeId, hideEyeId, evt) {
   }
 }
 
-//маска для инпута с датой рождения
-var dateMask = IMask(
-  document.getElementById('date-mask'),
-  {
-    mask: Date,
-    min: new Date(1900, 0, 1),
-    max: new Date(2050, 0, 1),
-    lazy: true,
-  });
+//======================Функция чтобы выбирался всегда только один чекбокс==============//
 
-const fff = () => {
-  const rr = document.getElementById('date-mask-group');
-
-  rr.addEventListener('click', () => {
-    var newMask = IMask(
-      document.getElementById('date-mask'),
-      {
-        mask: Date,
-        min: new Date(1900, 0, 1),
-        max: new Date(2050, 0, 1),
-        lazy: false,
-        placeholderChar: "_"
-      });
-  })
-};
-
-fff()
-
-var phoneMask = IMask(document.getElementById('phone-mask'), {
-  mask: '+{7}(000)000-00-00',
-  lazy: true,
-});
-
-const ddd = () => {
-  const rr = document.getElementById('phone-mask-group');
-
-  rr.addEventListener('click', () => {
-    var newphoneMask = IMask(document.getElementById('phone-mask'), {
-      mask: '+{7}(000)000-00-00',
-      lazy: false,
-      placeholderChar: '_'
-    });
-  })
-};
-
-ddd()
-
-function onlyOne(checkbox) {
-  var checkboxes = document.querySelectorAll('input[type=checkbox]')
-  checkboxes.forEach((item) => {
-      if (item !== checkbox) item.checked = false
-  })
-}
-
-})
+  function onlyOne(checkbox) {
+    var checkboxes = document.querySelectorAll('input[type=checkbox]')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+  }
